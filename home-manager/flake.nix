b9aa4cc83ae63195b87b3edc309184015b627377
benev0 <benjaminevanownby@gmail.com>
2026-06-03 07:03:54 -0400

    update for system version

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
